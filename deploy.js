003deca9cf629f16831c0ec7aaa93d7f223ef97c
moonbagel35 <47198079+moonbagel35@users.noreply.github.com>
2025-04-01 09:40:14 -0400

    sproutbot fork

--- deploy.js
+++ deploy.js
@@ -3,6 +3,7 @@ const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
+
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
